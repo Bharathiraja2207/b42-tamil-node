275db17d4759b28b7de1e4753e96edbc8bbe5503
Bharathiraja2207 <bharathiraja2207@gmail.com>
2023-03-01 18:30:27 +0530

    cors

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ import { MongoClient } from 'mongodb'
 import * as dotenv from 'dotenv'
 import moviesidRouter from './router/moviesid.router.js';
 import useridRouter from './router/userid.router.js'; //we can change name
+import cors from "cors";
 // import bcrypt   from ' bcrypt'
 dotenv.config()
 const app = express();
@@ -17,6 +18,8 @@ const mongo_url =(process.env.mongo_url)
 export const client = new MongoClient(mongo_url);
 await client.connect();
   console.log('mongo is connected!!');
+
+ app.use(cors())
 
   app.use(express.json())
 
--- router/userid.router.js
+++ router/userid.router.js
@@ -22,7 +22,7 @@ router.get("",async function (request, response) {
          const result= await client
             .db("userid")
            .collection("userids")
-            .insertMany(data)
+            .insertOne(data)
         response.send(result);
       });
   
@@ -43,6 +43,22 @@ router.get("",async function (request, response) {
           response.send(result);
         });
   
+        router.get("/:id", async function (request, response) {
+          const {id}=request.params;
+          console.log(id);
+          // const movies=movie.filter((mv)=>mv.id===id);
+      
+          // db.moviesid.findone({id:"100"})
+          
+          // const movies=movie.find((mv)=>mv.id===id);
+          const find_id= await client
+          .db("userid")
+          .collection("userids")
+          .findOne({id:id})
+          console.log(find_id);
+          find_id?response.send(find_id):response.status(404).send({message:"movie not found"})
+        });
+
         router.delete("/:id", async function (request, response) {
           const {id}=request.params;
          console.log(id)
